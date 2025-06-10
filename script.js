7f20db8488a58bc6d6aa7f424c82b0d4ab685941
Rohilian <160072663+Rohilian@users.noreply.github.com>
2025-06-10 09:14:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,3 @@ document.querySelector(".menu-btn").addEventListener("click", function() {
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.left = sidebar.style.left === "0px" ? "-250px" : "0px";
 });
-

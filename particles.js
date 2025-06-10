608a541f93ae50bac3f9c4dc7596d0815c482663
Rohilian <160072663+Rohilian@users.noreply.github.com>
2025-06-10 13:33:53 -0400

    Create particles.js

---
+++ particles.js
@@ -0,0 +1,59 @@
+const canvas = document.getElementById("particleCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const particlesArray = [];
+
+class Particle {
+    constructor(x, y, size, color) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if (this.size > 0.2) this.size -= 0.1;
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function createParticles(event) {
+    for (let i = 0; i < 5; i++) {
+        particlesArray.push(new Particle(event.x, event.y, Math.random() * 5 + 1, "white"));
+    }
+}
+
+function animateParticles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particlesArray.forEach((particle, index) => {
+        particle.update();
+        particle.draw();
+
+        if (particle.size <= 0.3) {
+            particlesArray.splice(index, 1);
+        }
+    });
+    requestAnimationFrame(animateParticles);
+}
+
+window.addEventListener("mousemove", createParticles);
+animateParticles();
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
